94ee8875a4690a9a362cba98f37896296df7a7e5
Petros Ypsilantis <pet_yps@hotmail.com>
2025-01-24 00:37:25 +0000

    add functionality for the front end api

---
+++ frontend/search-react-app/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SearchApp from './components/SearchApp.tsx';
+
+function App() {
+  return (
+    <div>
+      <SearchApp />
+    </div>
+  );
+}
+
+export default App;
